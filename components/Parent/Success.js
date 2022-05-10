efb6bfee75219e1cdab7667f46c6eff439c37252
Naboni <se.naboni.abebe@gmail.com>
2022-05-10 06:20:11 +0300

    before host

--- components/Parent/Success.js
+++ components/Parent/Success.js
@@ -1,15 +1,13 @@
-import Footer from "./Footer"
-import Header from "../HomePageComponents/Header"
-import Container from "./Container"
-import SuccessMain from "./SuccessMain"
+import Header from '../Common/Header'
+import Footer from './Footer'
+import SuccessMain from './SuccessMain'
 
 export default function Success() {
   return (
-    <div className=" shadow-lg px-1 md:px-10 lg:px-44 xl:px-64 2xl:px-96 mt-44  ">
-    <Header/>
-    <SuccessMain/>
-    <Footer/>
+    <div className=" mt-44 px-1 shadow-lg md:px-10 lg:px-44 xl:px-64 2xl:px-96  ">
+      <Header />
+      <SuccessMain />
+      <Footer />
     </div>
-  
   )
 }
