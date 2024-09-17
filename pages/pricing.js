e1f9cdacad1fc52a2046e3e066e4451598df13a9
Habtamu Asefa <nazrihabtish@gmail.com>
2024-09-17 11:20:53 +0300

    Updated pricing for 2024/25 school year

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,46 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import Header from '@/components/Common/Header'
 import Footer from '@/components/Common/Footer'
+import Header from '@/components/Common/Header'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Alert,
-  Box,
-  Item,
-  Button,
-  Tabs,
-  Checkbox,
-  Container,
-  FormHelperText,
-  Link,
-  Tab,
-  Typography,
-  Grid,
-  MenuItem,
-  InputLabel,
-  Select,
-  OutlinedInput,
-  Chip,
-} from '@mui/material'
+import { Alert, Grid, InputLabel, MenuItem, Select } from '@mui/material'
 const Pricing = () => {
   const router = useRouter()
   const [grade, setGrade] = useState('')
   const [hours, setHours] = useState(0)
   const [days, setDays] = useState(0)
   const [cost, setCost] = useState(0)
+
+  const WEEKS_PER_MONTH = 4
+
+  const price = (grade) => {
+    if (grade === 'KG') {
+      return 375
+    } else if (grade === 'COLLEGE') {
+      return 500
+    } else {
+      const gradeNumber = parseInt(grade, 10)
+      if (gradeNumber >= 1 && gradeNumber <= 5) {
+        return 375
+      } else if (gradeNumber >= 6 && gradeNumber <= 10) {
+        return 400
+      } else if (gradeNumber >= 11 && gradeNumber <= 12) {
+        return 425
+      } else {
+        return 0
+      }
+    }
+  }
+
+  useEffect(() => {
+    const calculatedCost = days * hours * WEEKS_PER_MONTH * price(grade)
+    setCost(calculatedCost)
+  }, [days, hours, grade])
+
   const handleDays = (event) => {
     setDays(Number(event.target.value))
-    setCost(event.target.value * hours * 4 * 300)
   }
+
   const handleHours = (event) => {
     setHours(Number(event.target.value))
-    setCost(event.target.value * days * 4 * 300)
   }
+
   const handleGrade = (event) => {
     setGrade(event.target.value)
   }
@@ -50,24 +54,36 @@ const Pricing = () => {
       <div className="h-screen overflow-y-scroll  bg-gray-50 font-minionPro scrollbar-hide">
         <Header />
         <div className="mt-16 px-10 xl:px-16">
-          <div className="my-2 p-3 md:p-5">
-            <h1 className="mt-12 text-center text-lg md:text-3xl ">Pricing</h1>
+          <div className="my-2 gap-3 p-3 md:p-5">
+            <h1 className="mt-12 text-center text-lg md:text-3xl ">
+              Pricing: [UPDATED FOR 2024/25 SCHOOL YEAR]
+            </h1>
             <h2 className="text-lg md:py-2 md:text-2xl">Pricing Per Hour</h2>
-            <div className="text-lg">
+            <div className="gap-3 text-lg">
               <p>
-                Sessions are 300 ETB per hour for KG-12 students for direct 1:1,
-                tutor- student time.
+                Sessions are in ETB per hour for direct 1:1, tutor- student
+                time.
               </p>
               <div>
-                For KG-12:
+                For KG-5:
                 <br />
-                300 ETB Birr Per Hour
+                375 ETB Birr Per Hour
               </div>
-              {/* <div>
-                For Freshman and Sophomore College Students:
+              <div>
+                For 6-10:
                 <br />
                 400 ETB Birr Per Hour
-              </div> */}
+              </div>
+              <div>
+                For 11-12:
+                <br />
+                400 ETB Birr Per Hour
+              </div>
+              <div>
+                For Freshman and Sophomore College Students:
+                <br />
+                500 ETB Birr Per Hour
+              </div>
             </div>
           </div>
 
@@ -113,6 +129,7 @@ const Pricing = () => {
                   fullWidth
                   onChange={(event) => handleGrade(event)}
                 >
+                  <MenuItem value="KG">KG</MenuItem>
                   <MenuItem value="1">G-1</MenuItem>
                   <MenuItem value="2">G-2</MenuItem>
                   <MenuItem value="3">G-3</MenuItem>
@@ -125,6 +142,7 @@ const Pricing = () => {
                   <MenuItem value="10">G-10</MenuItem>
                   <MenuItem value="11">G-11</MenuItem>
                   <MenuItem value="12">G-12</MenuItem>
+                  <MenuItem value="COLLEGE">College</MenuItem>
                 </Select>
               </Grid>
 
@@ -192,10 +210,17 @@ const Pricing = () => {
           </div>
           <div className=" md: mb-3 p-12">
             <Alert severity="info" sx={{ padding: '2' }}>
-              <p className="m-0 font-minionPro text-lg">
-                Your estimated monthly value is going to be
-                <label className="bg-[#fdd507] px-2 ">{cost}</label> birr
-              </p>
+              {hours * days * 4 < 12 ? (
+                <p className="m-0 font-minionPro text-lg text-red-500">
+                  The total number of hours per month must not be lower than 12
+                  hours for us to show tangible progress.
+                </p>
+              ) : (
+                <p className="m-0 font-minionPro text-lg">
+                  Your estimated monthly value is going to be
+                  <label className="bg-[#fdd507] px-2 ">{cost}</label> birr
+                </p>
+              )}
             </Alert>
           </div>
           <div className="my-4 flex justify-center">
