447d3be89e81aca97b49f4db466a90932b21e175
Eba Adisu <ebaadisu2@gmail.com>
2024-12-03 21:13:16 +0300

    Refactor PricingCard component styles

--- components/Pricing/PricingCard.tsx
+++ components/Pricing/PricingCard.tsx
@@ -7,14 +7,18 @@ interface PricingCardProps {
 
 const PricingCard: React.FC<PricingCardProps> = ({ title, price }) => {
   return (
-    <div className="flex h-48 w-full max-w-xs flex-col justify-evenly rounded-lg bg-white p-6 text-center shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-xl md:h-56 md:w-72">
-      {/* <h3 className="text-xl font-extrabold text-yellow-500 md:text-2xl"> */}
-      <h3 className="text-2xl font-extrabold text-[#1A3765] md:text-2xl">
+    <div className="flex h-56 w-full max-w-xs flex-col justify-between rounded-lg bg-white p-6 text-center shadow-xl transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-yellow-50 hover:shadow-2xl md:h-72 md:w-80">
+      <h3 className="mb-4 text-3xl font-extrabold text-[#1A3765] md:text-4xl">
         {title}
       </h3>
-      <p className=" text-xl font-semibold text-[#1A3765] md:text-xl">
+      <p className="mb-4 text-2xl font-semibold text-[#1A3765] md:text-3xl">
         {price} ETB per hour
       </p>
+      {/* <div className="mt-auto">
+        <span className="text-sm font-semibold text-yellow-500">
+          Limited Offer
+        </span>
+      </div> */}
     </div>
   )
 }
