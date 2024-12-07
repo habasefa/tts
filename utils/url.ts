005f88b98653aea8bf9df20ee49c7b6ccdfd3f1a
Eba Adisu <ebaadisu2@gmail.com>
2024-12-07 10:19:54 +0300

    Fix token expiration issue and update API URL

--- utils/url.ts
+++ utils/url.ts
@@ -1,8 +1,7 @@
-export const API_URL ='https://tts-web-api-259g.onrender.com/'
+// export const API_URL = 'https://tts-web-api-259g.onrender.com/'
+export const API_URL = 'http://localhost:4000/'
 
-'http://localhost:4000/'
-
-
+;('http://localhost:4000/')
 
 // 'http://localhost:3000/'
 
